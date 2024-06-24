c4192b13075f432ebfa1c7ef4ae6f01251373c2c
Cypress <cypress@lumafield.com>
2024-06-24 16:18:47 -0400

    Visualize basis vectors of a 2d coordinate system

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 
+// DOM elements
 const planeConstantSlider = document.querySelector<HTMLInputElement>(
   "input#plane-constant"
 )!;
@@ -14,6 +15,7 @@ const planeNormalZSlider = document.querySelector<HTMLInputElement>(
 )!;
 const canvas = document.querySelector("canvas")!;
 
+// Three.js setup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -21,14 +23,39 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-
+camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(window.innerWidth, window.innerHeight);
 
+// Plane
 const plane = new THREE.Plane(new THREE.Vector3(1, 1, 1).normalize(), -1);
 const planeHelper = new THREE.PlaneHelper(plane, 1, 0xffff00);
 scene.add(planeHelper);
 
+// Basis vectors that comprise a 2D coordinate system on the plane
+let basis1 = new THREE.Vector3();
+let basis2 = new THREE.Vector3();
+let helperVector =
+  Math.abs(plane.normal.x) > Math.abs(plane.normal.y)
+    ? new THREE.Vector3(0, 1, 0)
+    : new THREE.Vector3(1, 0, 0);
+basis1.crossVectors(plane.normal, helperVector).normalize();
+basis2.crossVectors(plane.normal, basis1).normalize();
+const basis1Helper = new THREE.ArrowHelper(
+  basis1,
+  new THREE.Vector3(),
+  1,
+  0xff0000
+);
+const basis2Helper = new THREE.ArrowHelper(
+  basis2,
+  new THREE.Vector3(),
+  1,
+  0x00ff00
+);
+scene.add(basis1Helper, basis2Helper);
+
+// Normal vector
 const normalHelper = new THREE.ArrowHelper(
   plane.normal,
   plane.normal.clone().multiplyScalar(-plane.constant),
@@ -37,22 +64,18 @@ const normalHelper = new THREE.ArrowHelper(
 );
 scene.add(normalHelper);
 
+// Position vector
 const planePositionHelper = new THREE.ArrowHelper(
   plane.normal,
-  new THREE.Vector3(0, 0, 0),
+  new THREE.Vector3(),
   Math.abs(plane.constant),
   0xffffff,
   0
 );
 scene.add(planePositionHelper);
 
-camera.position.z = 5;
-
-function animate() {
-  renderer.render(scene, camera);
-}
-
-function updateArrow() {
+// User input event listeners
+function updateVectors() {
   normalHelper.setDirection(plane.normal);
   normalHelper.position.copy(
     plane.normal.clone().multiplyScalar(-plane.constant)
@@ -60,8 +83,11 @@ function updateArrow() {
   planePositionHelper.setDirection(
     plane.normal.clone().multiplyScalar(-plane.constant).normalize()
   );
-  planePositionHelper.position.set(0, 0, 0);
   planePositionHelper.setLength(Math.abs(plane.constant), 0);
+  basis1.crossVectors(plane.normal, helperVector).normalize();
+  basis2.crossVectors(plane.normal, basis1).normalize();
+  basis1Helper.setDirection(basis1);
+  basis2Helper.setDirection(basis2);
 }
 
 planeConstantSlider.value = plane.constant.toString();
@@ -72,28 +98,28 @@ planeNormalZSlider.value = plane.normal.z.toString();
 planeConstantSlider.addEventListener("input", (e) => {
   const value = (e.target as HTMLInputElement).valueAsNumber;
   plane.constant = value;
-  updateArrow();
+  updateVectors();
 });
 
 planeNormalXSlider.addEventListener("input", (e) => {
   const value = (e.target as HTMLInputElement).valueAsNumber;
   plane.normal.x = value;
   plane.normal.normalize();
-  updateArrow();
+  updateVectors();
 });
 
 planeNormalYSlider.addEventListener("input", (e) => {
   const value = (e.target as HTMLInputElement).valueAsNumber;
   plane.normal.y = value;
   plane.normal.normalize();
-  updateArrow();
+  updateVectors();
 });
 
 planeNormalZSlider.addEventListener("input", (e) => {
   const value = (e.target as HTMLInputElement).valueAsNumber;
   plane.normal.z = value;
   plane.normal.normalize();
-  updateArrow();
+  updateVectors();
 });
 
-renderer.setAnimationLoop(animate);
+renderer.setAnimationLoop(() => renderer.render(scene, camera));
